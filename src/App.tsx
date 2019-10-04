746eaacd6489a2c2351d39670cc980bfa5456890
Carlos Alba <soradev4@gmail.com>
2019-10-04 13:44:29 -0700

    route set

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {  BrowserRouter, Route   } from 'react-router-dom';
+import { Landing } from './components/Landing';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Route exact path='/' component={Landing} />
     </BrowserRouter>
   );
 }
