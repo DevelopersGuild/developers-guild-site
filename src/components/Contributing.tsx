3fb01d3172409d4c32e7547668a537dc3116b8eb
Carlos Alba <soradev4@gmail.com>
2019-10-04 15:16:12 -0700

    contribution page

--- src/components/Contributing.tsx
+++ src/components/Contributing.tsx
@@ -222,6 +222,28 @@ export const Contributing: React.FC = () => (
             Do your homework beforehand. It’s OK not to know things, but show that you tried. Before asking for help, be sure to check a project’s README, documentation, issues (open or closed), mailing list, and search the internet for an answer. People will appreciate when you demonstrate that you’re trying to learn.
             </p>
 
+            <p className="text-justify">
+            Keep requests short and direct. Much like sending an email, every contribution, no matter how simple or helpful, requires someone else’s review. Many projects have more incoming requests than people available to help. Be concise. You will increase the chance that someone will be able to help you.
+            </p>
+
+            <p className="text-justify">
+            Keep all communication public. Although it’s tempting, don’t reach out to maintainers privately unless you need to share sensitive information (such as a security issue or serious conduct violation). When you keep the conversation public, more people can learn and benefit from your exchange. Discussions can be, in themselves, contributions.
+            </p>
+
+            <p className="text-justify">
+            It’s okay to ask questions (but be patient!). Everybody was new to the project at some point, and even experienced contributors need to get up to speed when they look at a new project. By the same token, even longtime maintainers are not always familiar with every part of the project. Show them the same patience that you’d want them to show to you.
+            </p>
+
+            <p className="text-justify">
+            Respect community decisions. Your ideas may differ from the community’s priorities or vision. They may offer feedback or decide not to pursue your idea. While you should discuss and look for compromise, maintainers have to live with your decision longer than you will. If you disagree with their direction, you can always work on your own fork or start your own project.
+            </p>
+
+            <p className="text-justify">
+            Above all, keep it classy. Open source is made up of collaborators from all over the world. Context gets lost across languages, cultures, geographies, and time zones. In addition, written communication makes it harder to convey a tone or mood. Assume good intentions in these conversations. It’s fine to politely push back on an idea, ask for more context, or further clarify your position. Just try to leave the internet a better place than when you found it.
+            </p>
+            
+
+
         </Container>
     </React.Fragment>
 )
