b3d71d35e5a9e7b279b173f3acf7cadf9fd16545
Carlos Alba <soradev4@gmail.com>
2019-10-04 15:27:23 -0700

    content

--- src/components/Contributing.tsx
+++ src/components/Contributing.tsx
@@ -292,7 +292,18 @@ export const Contributing: React.FC = () => (
             A pull request doesn’t have to represent finished work. It’s usually better to open a pull request early on, so others can watch or give feedback on your progress. Just mark it as a “WIP” (Work in Progress) in the subject line. You can always add more commits later.
             </p>
 
-            
+            <p className="text-justify">
+            If the project is on GitHub, here’s how to submit a pull request:
+            </p>
+
+            <ul>
+                <li>Fork the repository and clone it locally. Connect your local to the original “upstream” repository by adding it as a remote. Pull in changes from “upstream” often so that you stay up to date so that when you submit your pull request, merge conflicts will be less likely.</li>
+                <li>Create a branch for your edits.</li>
+                <li>Reference any relevant issues or supporting documentation in your PR (for example, “Closes #37.”)</li>
+                <li>Include screenshots of the before and after if your changes include differences in HTML/CSS. Drag and drop the images into the body of your pull request.</li>
+                <li>Test your changes! Run your changes against any existing tests if they exist and create new ones when needed. Whether tests exist or not, make sure your changes don’t break the existing project.</li>
+                <li>Contribute in the style of the project to the best of your abilities. This may mean using indents, semi-colons or comments differently than you would in your own repository, but makes it easier for the maintainer to merge, others to understand and maintain in the future.</li>
+            </ul>
 
         </Container>
     </React.Fragment>
