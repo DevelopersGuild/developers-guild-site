4054177a8e1b0deaccc5ab2b78b69ba340aae95b
Carlos Alba <soradev4@gmail.com>
2019-10-04 13:41:22 -0700

    react router dom types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import {    } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-        
+
     </div>
   );
 }
