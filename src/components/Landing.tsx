a9db3945ba3d6f21560949c6395a68a6acf7ed60
Carlos Alba <soradev4@gmail.com>
2019-10-04 13:50:07 -0700

    navbar

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,14 @@
-import React from 'react'; 
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 export const Landing: React.FC = () => {
-    return(
+    return (
         <React.Fragment>
-            
+            <Navbar bg="light">
+                <Navbar.Brand href="#home">Developers Guild</Navbar.Brand>
+            </Navbar>
+            <Container>
+            </Container>
         </React.Fragment>
     )
 }
