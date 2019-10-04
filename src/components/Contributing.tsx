060b55e853eef6495928de93a2e16d730a70f2d6
Carlos Alba <soradev4@gmail.com>
2019-10-04 14:26:30 -0700

    contributions

--- src/components/Contributing.tsx
+++ src/components/Contributing.tsx
@@ -3,9 +3,12 @@ import { Container, } from 'react-bootstrap';
 
 export const Contributing: React.FC = () => (
     <React.Fragment>
-        <br/>
+        <br />
         <Container>
-            <h1>Contributing</h1>
+            <h4>How to Contribute to Open Source Developers Guild Projects</h4>
+            <p>
+                Want to contribute to open source? A guide to making open source contributions, for first-timers and for veterans.
+            </p>
         </Container>
     </React.Fragment>
 )
