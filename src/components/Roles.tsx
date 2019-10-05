4ed519be3d088747c8e02a460f07e99ad5e01481
Carlos Alba <soradev4@gmail.com>
2019-10-04 20:22:20 -0700

    roles

--- src/components/Roles.tsx
+++ src/components/Roles.tsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import showdown from 'showdown';
 
 
 const RolesRaw: React.FC = () => {
 
+    const [markDown, setMarkDown] = useState<any>();
+
     useEffect(() => {
-        
+        (async () => {
+            const response = await (await fetch('https://raw.githubusercontent.com/DevelopersGuild/developers-guild-site-roles/master/Roles.md')).arrayBuffer();
+            const stringParsed = new TextDecoder("utf-8").decode(response);
+            const showdownMarkdown = new showdown.Converter().makeHtml(stringParsed);
+            setMarkDown(showdownMarkdown);
+        })();
     })
 
-    return(
+    return (
         <React.Fragment>
-            <Container>
-
+            <br/>
+            <Container className='role-container'>
+                <div dangerouslySetInnerHTML={{ __html: markDown }} ></div>
             </Container>
         </React.Fragment>
     )
