3fceca480d0b933a7f6a21a5da0cbe042dcfc2a7
Carlos Alba <soradev4@gmail.com>
2019-10-04 19:56:35 -0700

    roles

---
+++ src/components/Roles.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+
+
+const RolesRaw: React.FC = () => {
+    return(
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+
+/**
+ * Memoizing component to prevent useless re-rendering
+ */
+const Roles = React.memo(RolesRaw);
+
+export default Roles;
