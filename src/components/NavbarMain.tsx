4ffb52c7e8bf3ea5058b86077133335eed0f8317
Carlos Alba <soradev4@gmail.com>
2019-10-04 20:35:13 -0700

    collapsing menu

--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -4,22 +4,26 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 export const NavbarMain: React.FC = () => (
     <React.Fragment>
-        <Navbar bg="light">
+        <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/">Developers Guild</Navbar.Brand>
-            <Nav>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2" href="/contributing">Contributing</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-3" href="/contact" >Contact Us</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-4" href="/roles" >Roles</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2" href="/contributing">Contributing</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-3" href="/contact" >Contact Us</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-4" href="/roles" >Roles</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+
         </Navbar>
     </React.Fragment>
 );
