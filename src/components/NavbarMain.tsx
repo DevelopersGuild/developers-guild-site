7ced934750dbf96de6d2314fdec35bd5c0d7f470
Carlos Alba <soradev4@gmail.com>
2019-10-06 12:42:31 -0700

    navlink dynamic

--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -10,16 +10,16 @@ export const NavbarMain: React.FC = () => (
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Item>
-                        <Nav.Link eventKey="link-1" href="#/about">About</Nav.Link>
+                        <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-2" href="#/contributing">Contributing</Nav.Link>
+                        <Nav.Link eventKey="link-2" href="/contributing">Contributing</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-3" href="#/contact" >Contact Us</Nav.Link>
+                        <Nav.Link eventKey="link-3" href="/contact" >Contact Us</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-4" href="#/roles" >Roles</Nav.Link>
+                        <Nav.Link eventKey="link-4" href="/roles" >Roles</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
