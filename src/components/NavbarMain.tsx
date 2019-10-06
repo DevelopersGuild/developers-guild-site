0bcb5dba6e3fdd86df24169688bec75c05c6d205
Carlos Alba <soradev4@gmail.com>
2019-10-06 12:59:06 -0700

    navbar changes

--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -5,22 +5,22 @@ import { Link } from 'react-router-dom';
 
 export const NavbarMain: React.FC = () => (
     <React.Fragment>
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="/">Developers Guild</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Item>
-                        <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                        <Link className="nav-link" to="/about">About</Link>
                     </Nav.Item>
                     <Nav.Item>
-                            <Nav.Link><Link to="/contributing">Contributing</Link></Nav.Link>
+                        <Link className="nav-link" to="/contributing">Contributing</Link>
                     </Nav.Item>
                     <Nav.Item>
-                       <Nav.Link><Link to="/contact">Contact Us</Link></Nav.Link>
+                        <Link className="nav-link" to="/contact">Contact Us</Link>
                     </Nav.Item>
                     <Nav.Item>
-                            <Nav.Link><Link to="/roles">Roles</Link></Nav.Link>
+                        <Link className="nav-link" to="/roles">Roles</Link>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
