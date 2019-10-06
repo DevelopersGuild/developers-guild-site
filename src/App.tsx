130615d5ce2f397dbd5cae638952e618fb622351
Carlos Alba <soradev4@gmail.com>
2019-10-06 12:30:38 -0700

    routes hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App: React.FC = () => {
     <HashRouter>
       <NavbarMain />
       <Route exact path='/' component={Landing} />
-      <Route exact path='/contact' component={Contact} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/contributing' component={Contributing} />
-      <Route exact path='/roles' component={Roles} />
+      <Route exact path='contact' component={Contact} />
+      <Route exact path='about' component={About} />
+      <Route exact path='contributing' component={Contributing} />
+      <Route exact path='roles' component={Roles} />
     </HashRouter>
   );
 }
