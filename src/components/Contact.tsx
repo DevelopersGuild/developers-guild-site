34b11f6a2ff7ef718624c7a62db806354201ceec
Carlos Alba <soradev4@gmail.com>
2019-10-06 17:08:49 -0700

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,9 +4,17 @@ import { Container } from 'react-bootstrap';
 
 export const Contact: React.FC = () => (
     <React.Fragment>
-        <br/>
-        <Container>
-                <h1>Contact Us</h1>
-        </Container>
+          <br />
+            <div style={{ display: "flex", flexDirection: 'column' }} >
+                <Container>
+                    <h4>Contact The Developers Guild</h4>
+                    <p className="text-justify">
+                        Here are different ways to contact the developers guild.
+                    </p>
+                </Container>
+                {/* <img className="open-source-header" style={{ width: '30vw', alignSelf: 'center' }} src={RolesHeader} alt="Header" /> */}
+                <Container className='role-container'>
+                </Container>
+            </div>
     </React.Fragment>
 )
