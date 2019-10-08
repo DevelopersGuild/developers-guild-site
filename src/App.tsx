bf240ab01b4291441c9600d34998f403f8f364fe
Carlos Alba <soradev4@gmail.com>
2019-10-08 14:19:32 -0700

    Constitution Routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { NavbarMain } from './components/NavbarMain';
 import { About } from './components/About';
 import { Contributing } from './components/Contributing';
 import Roles from './components/Roles';
+import Constitution from './components/Constitution';
 
 
 const App: React.FC = () => {
@@ -13,10 +14,11 @@ const App: React.FC = () => {
     <BrowserRouter>
       <NavbarMain />
       <Route path='/' exact component={Landing} />
-      <Route path='/contact'exact component={Contact} />
+      <Route path='/contact' exact component={Contact} />
       <Route path='/about' exact component={About} />
       <Route path='/contributing' exact component={Contributing} />
       <Route path='/roles' exact component={Roles} />
+      <Route path="/roles" exact component={Constitution} />
     </BrowserRouter>
   );
 }
