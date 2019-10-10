92e935f5f6119d2350e3599fdce07675732f2aef
Carlos Alba <soradev4@gmail.com>
2019-10-09 17:46:50 -0700

    about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,22 @@ export const About: React.FC = () => (
             </Container>
             <img className="open-source-header lazyload" style={{ width: '30vw', alignSelf: 'center' }} data-src={AboutHeader} alt="Header" />
             <Container>
-                
+                <br />
+                <h5>Section 1</h5>
+                <p className="text-justify">
+                Developers’ Guild is a club that started in 2014 with its main goal is to facilitate the exploration of computer science through hands-on experience. Developers’ Guild creates a casual environment to practice real-world programming. We foster a community of avid programmers by tackling projects that interest us, competing/participating in events, and gathering skills and experience for the future. Our members are divided into various groups. Each group focuses on a different aspect of computer science. Video Game Development, Machine Learning/AI, and Web application development are some of the groups we provide. Members in these groups will learn these computer science concepts and create projects to apply what they learned. We have various projects for all skill levels ranging from beginner to more experienced programming projects. When you join DG and attend our meetings, you can help contribute code to existing and new projects that you can then put on your portfolio for future jobs and even transfer. Learn new concepts with a community of people that share a passion for programming. 
+                </p>
+                <br/>
+                <h5>TL;DR</h5>
+                <ul>
+                    <li>Provide a good way to get real-world programming experience.</li>
+                    <li>To facilitate the exploration of computer science through hands-on experience.</li>
+                    <li>Adept Programmers can work on new challenges and mentor others.</li>
+                    <li>Open Source Projects for your portfolio and resume!</li>
+                    <li>Collaboration with individuals with differnent skills, perspectives and backgrounds.</li>
+                    <li>Communication with Mentors, Industry Leaders and Speakers.</li>
+                </ul>
+                <br/>
             </Container>
         </div>
     </React.Fragment>
