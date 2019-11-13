486e467ea82f0522136c9ce5668e95c679f75d8b
Carlos Alba <soradev4@gmail.com>
2019-11-12 16:48:07 -0800

    change

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, TextInput, Button, Text, Grommet, grommet } from 'grommet';
+import { Box, Heading, TextInput, Button, Text, Grommet, grommet, Grid } from 'grommet';
 
 const Login: React.FC = () => {
     const [email, setEmail] = React.useState('');
@@ -11,33 +11,35 @@ const Login: React.FC = () => {
 
     return (
         <Grommet full theme={grommet}>
-            <Box justify="start" align="center" width="medium" direction="column" pad="medium" round="small" border={{ color: 'grey', size: 'large' }} >
-                <Heading margin="none">Welcome Back</Heading>
-                <br />
-                <Box width="medium">
-                    <TextInput
-                        placeholder="email"
-                        value={email}
-                        type="email"
-                        onChange={event => setEmail(event.target.value)}
-                    />
+            <Grid>
+                <Box justify="start" align="center" width="medium" direction="column" pad="medium" round="small" border={{ color: 'grey', size: 'large' }} >
+                    <Heading margin="none">Welcome Back</Heading>
+                    <br />
+                    <Box width="medium">
+                        <TextInput
+                            placeholder="email"
+                            value={email}
+                            type="email"
+                            onChange={event => setEmail(event.target.value)}
+                        />
+                    </Box>
+                    <br />
+                    <Box width="medium">
+                        <TextInput
+                            placeholder="password"
+                            value={password}
+                            type="password"
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                    </Box>
+                    <br />
+                    <Box width="medium" >
+                        <Button primary label="Primary" onClick={handleSubmission} />
+                    </Box>
+                    <hr />
+                    <Text>Don't have an account? <a href="/signup" >Sign up</a></Text>
                 </Box>
-                <br />
-                <Box width="medium">
-                    <TextInput
-                        placeholder="password"
-                        value={password}
-                        type="password"
-                        onChange={event => setPassword(event.target.value)}
-                    />
-                </Box>
-                <br />
-                <Box width="medium" >
-                    <Button primary label="Primary" onClick={handleSubmission} />
-                </Box>
-                <hr />
-                <Text>Don't have an account? <a href="/signup" >Sign up</a></Text>
-            </Box>
+            </Grid>
         </Grommet>
     );
 }
