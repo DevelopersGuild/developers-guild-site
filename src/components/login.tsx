475e36791ef6a1c9ba3f331c9cd2eb7bb4d781ea
Carlos Alba <soradev4@gmail.com>
2019-11-12 16:47:06 -0800

    fmt

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -10,25 +10,34 @@ const Login: React.FC = () => {
     }
 
     return (
-        <Grommet theme={grommet}>
-                <Box direction="column" pad="medium" round="small" border={{ color: 'grey', size: 'large' }} >
-                    <Heading margin="none">Welcome Back</Heading>
+        <Grommet full theme={grommet}>
+            <Box justify="start" align="center" width="medium" direction="column" pad="medium" round="small" border={{ color: 'grey', size: 'large' }} >
+                <Heading margin="none">Welcome Back</Heading>
+                <br />
+                <Box width="medium">
                     <TextInput
                         placeholder="email"
                         value={email}
                         type="email"
                         onChange={event => setEmail(event.target.value)}
                     />
+                </Box>
+                <br />
+                <Box width="medium">
                     <TextInput
                         placeholder="password"
                         value={password}
                         type="password"
                         onChange={event => setPassword(event.target.value)}
                     />
-                    <Button primary label="Primary" onClick={handleSubmission} />
-                    <hr />
-                    <Text>Don't have an account? <a href="/signup" >Sign up</a></Text>
                 </Box>
+                <br />
+                <Box width="medium" >
+                    <Button primary label="Primary" onClick={handleSubmission} />
+                </Box>
+                <hr />
+                <Text>Don't have an account? <a href="/signup" >Sign up</a></Text>
+            </Box>
         </Grommet>
     );
 }
