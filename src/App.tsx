0bd7e4286cd076d1ee48628dc7e958d689802c41
Carlos Alba <soradev4@gmail.com>
2019-11-21 13:18:58 -0800

    listings page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from './components/landing';
 import Login from './components/login';
 import Signup from './components/signup';
 import ListingTemplate from './components/listing';
+import Listings from './components/listings';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ReactTooltip from 'react-tooltip'
 
@@ -16,6 +17,7 @@ const App: React.FC = () => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/example-listing" component={ListingTemplate} />
+        <Route exact path="/listings" component={Listings} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/listings.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import loftryTheme from './Theme';
+import { Grommet, Box, Carousel, Image, Heading, Text, Button, Anchor, grommet, InfiniteScroll } from 'grommet';
+import Thumbnail from './thumbnail';
+
+const Listings: React.FC = () => {
+    const test: number[] = [];
+    for (let i = 0; i < 1000; i++) {
+        test.push(i);
+    }
+    return (
+        <Grommet theme={loftryTheme}>
+            <Box align="center" justify="center">
+                <Heading>Listings</Heading>
+                <Box overflow="auto" align="center" justify="center" direction="row" wrap>
+                    <InfiniteScroll items={test}>
+                        {(item) => <Thumbnail key={item} />}
+                    </InfiniteScroll>
+                </Box>
+            </Box>
+        </Grommet>
+    )
+}
+
+export default Listings; 
