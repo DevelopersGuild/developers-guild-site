e448d5fb296d3d9636c980f6f3f0a86ac6984a96
Carlos Alba <soradev4@gmail.com>
2019-11-21 11:33:33 -0800

    landing page changes

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grommet, grommet, Box, Heading, Button } from 'grommet';
+import { Grommet, grommet, Box, Heading, Button, Tab, Tabs, Text } from 'grommet';
 
 
 const Landing: React.FC = () => {
@@ -14,10 +14,56 @@ const Landing: React.FC = () => {
                     </Box>
                 </Box>
             </Box>
-            <Box align="center" justify="center" fill >
-                <Heading>
-                    A new solution for Co-Living
-                </Heading>
+            <Box align="center" justify="start" fill >
+                <Box pad="small" align="center" >
+                    <Tabs>
+                        <Tab title="Privacy">
+                            <Box fill>
+                                <Heading alignSelf="start">Privacy</Heading>
+                                <Text>
+                                    Protecting User privacy is important to the Loftly
+                                    team this is why all A.I powered features are embedded
+                                    directly into the client. Basically Loftly runs all
+                                    A.I insights on a closed loop system directly on your device.
+                            </Text>
+                            </Box>
+                        </Tab>
+                        <Tab title="Automation">
+                            <Box fill >
+                                <Heading alignSelf="start">Workflow Automation</Heading>
+                                <Text>
+                                    Automating responses to Tenants and Third Parties and automating
+                                    actions such as approving of decisions of processing complaints
+                                    is a huge time saver which allows you and your team to focus on
+                                    the work that matters to you.
+                                </Text>
+                            </Box>
+                        </Tab>
+                        <Tab title="Modern Platform">
+                            <Box fill>
+                                <Heading alignSelf="start">Easy to Use Reliable Software</Heading>
+                                <Text>
+                                    Loftly is powered by the latest open source software
+                                    validated by thousands of organizations across the globe
+                                    like Google, Apple, Facebook and Others. Loftly is also built
+                                    on the fault tolerant hardware that all these companies run on.
+                                    It's a cloud native, cross platform tool for managing tenants and
+                                    properties. We also run thousands of automated tests to make sure every
+                                    feature on the platform works properly.
+                                </Text>
+                            </Box>
+                        </Tab>
+                        <Tab title="Thriving Habitat">
+                            <Box fill>
+                                <Heading alignSelf="start">Bootstrapping Eco-Systems</Heading>
+                                <Text>
+                                    Loftly makes interacting and planning around your roomates schedules
+                                    easier mediate the planning of social events, chores, bills and transportation.
+                                </Text>
+                            </Box>
+                        </Tab>
+                    </Tabs>
+                </Box>
             </Box>
         </Grommet>
     );
