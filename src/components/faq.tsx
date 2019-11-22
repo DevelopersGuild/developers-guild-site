748b9399ae52ce1969b563b8701769da8edc3594
Carlos Alba <soradev4@gmail.com>
2019-11-21 16:41:12 -0800

    faq

---
+++ src/components/faq.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import loftryTheme from './Theme';
+import { Grommet, Box, Text } from 'grommet';
+
+const Faq: React.FC = () => {
+    return (
+        <Grommet theme={loftryTheme}>
+            <Box>
+                <Text>Faq</Text>
+            </Box>
+        </Grommet>
+    )
+}
+
+export default Faq; 
