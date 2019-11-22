bf0359555d2d173ce1d952420e7de0e35c91f282
Carlos Alba <soradev4@gmail.com>
2019-11-21 16:42:43 -0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Login from './components/login';
 import Signup from './components/signup';
 import ListingTemplate from './components/listing';
 import Listings from './components/listings';
+import About from './components/about'; 
+import Faq from './components/faq';
+import Help from './components/help';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ReactTooltip from 'react-tooltip'
 
@@ -18,6 +21,7 @@ const App: React.FC = () => {
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/example-listing" component={ListingTemplate} />
         <Route exact path="/listings" component={Listings} />
+        <Route exact path="/about" />
       </Switch>
     </BrowserRouter>
   );
