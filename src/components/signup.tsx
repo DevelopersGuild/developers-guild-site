bc2596e25eb9f902943426aae60c3cd2fde8e6c9
Carlos Alba <soradev4@gmail.com>
2019-11-21 16:48:55 -0800

    changes

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -3,16 +3,18 @@ import { Grommet, grommet, Box, Text, TextInput } from 'grommet';
 
 const Signup: React.FC = () => {
     return (
-        <Grommet theme={grommet}>
-            <Box>
-                <Text>Sign Up</Text>
-                <br />
-                <Box>
-                    <TextInput placeholder="email" type="email" />
-                </Box>
-                <br />
-                <Box>
-                    <TextInput placeholder="password" type="password" />
+        <Grommet full theme={grommet}>
+            <Box fill align="center" justify="center">
+                <Box width="medium">
+                    <Text>Sign Up</Text>
+                    <br />
+                    <Box>
+                        <TextInput placeholder="email" type="email" />
+                    </Box>
+                    <br />
+                    <Box>
+                        <TextInput placeholder="password" type="password" />
+                    </Box>
                 </Box>
             </Box>
         </Grommet>
