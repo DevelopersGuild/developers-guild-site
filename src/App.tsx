95bc91379cc6dcd692d365b677b5ed51fc571e29
Carlos Alba <soradev4@gmail.com>
2019-11-21 16:43:50 -0800

    fmt

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Login from './components/login';
 import Signup from './components/signup';
 import ListingTemplate from './components/listing';
 import Listings from './components/listings';
-import About from './components/about'; 
+import About from './components/about';
 import Faq from './components/faq';
 import Help from './components/help';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
