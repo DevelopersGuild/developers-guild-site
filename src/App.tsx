4be0447a74b168dd800261625fba5024d9c0f462
Johnny To <tojohnny1559@gmail.com>
2019-11-22 08:55:49 -0800

    Added Profile Route

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,23 @@ import Landing from './components/landing';
 import Login from './components/login';
 import Signup from './components/signup';
 import ListingTemplate from './components/listing';
+import Listings from './components/listings';
+import Profile from './components/Profile';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip'
 
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <ReactTooltip />
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/myProfile" component={Profile} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/example-listing" component={ListingTemplate} />
+        <Route exact path="/listings" component={Listings} />
       </Switch>
     </BrowserRouter>
   );
