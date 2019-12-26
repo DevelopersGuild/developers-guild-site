6219f6dcbaa569f1d837e924da4ea69cc6bc2338
Carlos Alba <soradev4@gmail.com>
2019-12-26 12:53:29 -0800

    Change

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,53 +1,53 @@
 import React from 'react';
 import loftryTheme from './Theme';
-import { Box, Header,Layer, Heading, TextInput, Button, Text, Grommet, Anchor, Tabs, Tab, } from 'grommet';
-import {Login} from 'grommet-icons';
+import { Box, Header, Layer, Heading, TextInput, Button, Text, Grommet, Anchor, Tabs, Tab, } from 'grommet';
+import { Login } from 'grommet-icons';
 import Register from './Register';
 
 const Front: React.FC = () => {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    const handleSubmission = async () => {
-        console.log(JSON.stringify({ email, password }));
-    }
-    const [show, setShow] = React.useState();
+  const handleSubmission = async () => {
+    console.log(JSON.stringify({ email, password }));
+  }
+  const [show, setShow] = React.useState();
 
-    return (
-        <Grommet full theme={loftryTheme}>
-            <Box fill="vertical" overflow = "hidden"align="start" flex="grow" background="url(https://images.unsplash.com/43/L6sQn4GyQdSBW7pLgEz7_DSC_0013.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1945&q=80)" direction="column">
+  return (
+    <Grommet full theme={loftryTheme}>
+      <Box fill="vertical" overflow="hidden" align="start" flex="grow" background="url(https://images.unsplash.com/43/L6sQn4GyQdSBW7pLgEz7_DSC_0013.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1945&q=80)" direction="column">
         <Header align="center" direction="row-responsive" justify="start" gap="large" fill="horizontal">
-          <Heading textAlign="start" margin={{"left":"medium","right":"large"}}>
+          <Heading textAlign="start" margin={{ "left": "medium", "right": "large" }}>
             Loftry
           </Heading>
           <Box direction="row" gap="medium">
-            <Anchor href = "/about" label="About" size="large" />
-            <Anchor href = "/faq" label="FAQ" size="large" />
-            <Anchor href = "/help" label="Help" size="large" />
-            <Anchor href = "/example-profile" label="Profile" size="large" />
+            <Anchor href="/about" label="About" size="large" />
+            <Anchor href="/faq" label="FAQ" size="large" />
+            <Anchor href="/help" label="Help" size="large" />
+            <Anchor href="/example-profile" label="Profile" size="large" />
           </Box>
         </Header>
         <Box align="center" justify="center" direction="row" gap="medium">
-          <Box align="stretch" justify="start" fill={false} background={{"color":"background"}} pad="large" wrap={false} round="medium" gap="large" direction="column" margin="xlarge">
+          <Box align="stretch" justify="start" fill={false} background={{ "color": "background" }} pad="large" wrap={false} round="medium" gap="large" direction="column" margin="xlarge">
             <Heading textAlign="start" margin="small" size="medium" level="1">
               Welcome Back
             </Heading>
-            <TextInput placeholder="Email" value={email} type="email" onChange={event => setEmail(event.target.value)}/>
-            <TextInput placeholder="Password" type="password" value={password} onChange={event => setPassword(event.target.value)}/>
+            <TextInput placeholder="Email" value={email} type="email" onChange={event => setEmail(event.target.value)} />
+            <TextInput placeholder="Password" type="password" value={password} onChange={event => setPassword(event.target.value)} />
             <Button label="Login" color="brand" fill="horizontal" icon={<Login />} primary={true} onClick={handleSubmission} />
             <Text textAlign="start">
               Don't have an account? <Anchor onClick={() => setShow(true)}> Sign Up</Anchor>
             </Text>
             {show && (
-                            <Layer
-                                onEsc={() => setShow(false)}
-                                onClickOutside={() => setShow(false)}
-                            >
-                                <Register />
-                            </Layer>
-                        )}
+              <Layer
+                onEsc={() => setShow(false)}
+                onClickOutside={() => setShow(false)}
+              >
+                <Register />
+              </Layer>
+            )}
           </Box>
-          <Box align="start" justify="center" direction="column" margin={{"left":"large"}}>
+          <Box align="start" justify="center" direction="column" margin={{ "left": "large" }}>
             <Heading textAlign="start" color="white">
               Introducing Loftly Housing. <br />Welcome to AI Powered Co-living.
             </Heading>
@@ -58,11 +58,11 @@ const Front: React.FC = () => {
           </Box>
         </Box>
       </Box>
-            <Box tag="footer" pad="small" background="dark-1">
-            footer
+      <Box tag="footer" pad="small" background="dark-1">
+        footer
           </Box>
-        </Grommet>
-    )
+    </Grommet>
+  )
 }
 
 export default Front;
