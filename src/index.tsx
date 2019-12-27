327f737ba1a7d7cd2f4adcf04b83591b2012c329
Carlos Alba <soradev4@gmail.com>
2019-12-26 19:31:44 -0800

    change

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { config } from 'dotenv';
 import * as serviceWorker from './serviceWorker';
 
-config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
