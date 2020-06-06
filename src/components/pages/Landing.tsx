e78bba352396b63410d88a133f5fa6fdd6c5db40
Carlos Alba <soradev4@gmail.com>
2020-06-05 18:05:07 -0700

    GithubBar React Query Changes

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,32 +1,39 @@
-import React, { lazy, Suspense } from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
-import MainLogo from '../graphic-assets/Main-Logo.svg'
-const GithubBar = lazy(() => import('../ui/GithubBar'));
+import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
+import MainLogo from "../graphic-assets/Main-Logo.svg";
+import GithubBar from "../ui/GithubBar";
 
 export const Landing: React.FC = () => (
-    <React.Fragment>
-        <br />
-        <Container>
-            <Jumbotron>
-                <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center' }}>
-
-                    <img className="lazyload" style={{ marginRight: 20 }} data-src={MainLogo} alt="Logo" width="150"
-                        height="150"
-                    />
-                    <div>
-                        <h1>Developers Guild - De Anza</h1>
-                        <p>
-                            A project and contribution based programming club.
-                        </p>
-                        <p>Fridays 1:30PM-4:00PM AT311</p>
-                    </div>
-                </div>
-            </Jumbotron>
-            <br />
-            <Suspense fallback={<div>Loading...</div>}>
-                <GithubBar />
-            </Suspense>
-            <br />
-        </Container>
-    </React.Fragment>
-)
+  <React.Fragment>
+    <br />
+    <Container>
+      <Jumbotron>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            alignItems: "center",
+          }}
+        >
+          <img
+            className="lazyload"
+            style={{ marginRight: 20 }}
+            data-src={MainLogo}
+            alt="Logo"
+            width="150"
+            height="150"
+          />
+          <div>
+            <h1>Developers Guild - De Anza</h1>
+            <p>A project and contribution based programming club.</p>
+            <p>Fridays 1:30PM-4:00PM AT311</p>
+          </div>
+        </div>
+      </Jumbotron>
+      <br />
+      <GithubBar />
+      <br />
+    </Container>
+  </React.Fragment>
+);
--- src/components/ui/GithubBar.tsx
+++ src/components/ui/GithubBar.tsx
@@ -1,55 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
+import { useQuery } from "react-query";
 
-const GithubBarRaw: React.FC = () => {
+async function fetchRepos() {
+  const response = await (
+    await fetch(
+      "https://api.github.com/orgs/DevelopersGuild/repos?sort=updated"
+    )
+  ).json();
+  return response;
+}
 
-    const [repos, setRepos] = useState<any[]>([])
-
-    useEffect(() => {
-        (async () => {
-            const abortController = new AbortController();
-            const signal = abortController.signal;
-            const response = await (await fetch('https://api.github.com/orgs/DevelopersGuild/repos?sort=updated', { signal })).json();
-            setRepos(response);
-            return function cleanup() {
-                abortController.abort();
-            }
-        })();
-    }, []);
-
-    return (
-        <React.Fragment>
-            <h5>Github Repositories</h5>
-            <Card>
-                <Card.Body>
-                    <div>
-                        <div style={{ overflow: 'auto', maxHeight: 400 }}>
-                            {repos.map((e, index) => (
-                                <React.Fragment key={index}>
-                                    <Card.Body>
-                                        <Card.Title>{e.full_name}</Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">{e.description}</Card.Subtitle>
-                                        <Card.Text> {" "}
-                                            <span aria-label="forks" role="img">🍴</span> {" "} Forks:{e.forks_count} {" "}
-                                            <span aria-label="watchers" role="img">👁</span> {" "} Watchers:{e.watchers} {" "}
-                                            <span aria-label="language" role="img">💻</span> {" "} Language: {e.language}
-                                        </Card.Text>
-                                        <Card.Link href={e.html_url}>Repository</Card.Link>
-                                    </Card.Body>
-                                </React.Fragment>
-                            ))}
-                        </div>
-                        <br />
-                    </div>
-                </Card.Body>
-            </Card>
-        </React.Fragment>
-    );
-};
-
-/**
- * Memoizing component to prevent useless re-rendering
- */
-const GithubBar = React.memo(GithubBarRaw);
+function GithubBar(): JSX.Element {
+  const { status, data, error } = useQuery("github-repos", fetchRepos);
+  if (status === "loading") return <div>Loading...</div>;
+  if (status === "error") return <div>Error: {error}</div>;
+  return (
+    <React.Fragment>
+      <h5>Github Repositories</h5>
+      <Card>
+        <Card.Body>
+          <div>
+            <div style={{ overflow: "auto", maxHeight: 400 }}>
+              {data.map((e: any, index: any) => (
+                <React.Fragment key={index}>
+                  <Card.Body>
+                    <Card.Title>{e.full_name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {e.description}
+                    </Card.Subtitle>
+                    <Card.Text>
+                      {" "}
+                      <span aria-label="forks" role="img">
+                        🍴
+                      </span>{" "}
+                      Forks:{e.forks_count}{" "}
+                      <span aria-label="watchers" role="img">
+                        👁
+                      </span>{" "}
+                      Watchers:{e.watchers}{" "}
+                      <span aria-label="language" role="img">
+                        💻
+                      </span>{" "}
+                      Language: {e.language}
+                    </Card.Text>
+                    <Card.Link href={e.html_url}>Repository</Card.Link>
+                  </Card.Body>
+                </React.Fragment>
+              ))}
+            </div>
+            <br />
+          </div>
+        </Card.Body>
+      </Card>
+    </React.Fragment>
+  );
+}
 
 export default GithubBar;
