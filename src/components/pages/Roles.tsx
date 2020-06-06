d97e54be39d53b92cbc37ee868dc5f6d53c3d2f5
Carlos Alba <soradev4@gmail.com>
2020-06-05 18:19:50 -0700

    Roles React-Query

--- src/components/pages/Roles.tsx
+++ src/components/pages/Roles.tsx
@@ -1,60 +1,58 @@
-import React, { useState, useEffect } from 'react'; 
-import { Container } from 'react-bootstrap';
-import showdown from 'showdown';
-import RolesHeader from '../graphic-assets/roles.jpg'
-import '../styles/image-headers.css'
+import React from "react";
+import { Container } from "react-bootstrap";
+import showdown from "showdown";
+import RolesHeader from "../graphic-assets/roles.jpg";
+import "../styles/image-headers.css";
+import { useQuery } from "react-query";
 
-const RolesRaw: React.FC = () => {
-
-    const [markDown, setMarkDown] = useState<any>();
-
-    useEffect(() => {
-        (async () => {
-            /**
-             * Takes data from a Markdown file that's easy to edit through the developers guild github
-             * and parses it out on this page.
-             */
-            const abortController = new AbortController();
-            const signal = abortController.signal;
-            const response = await (await fetch('https://raw.githubusercontent.com/DevelopersGuild/developers-guild-site-roles/master/Roles.md', { signal })).arrayBuffer();
-            const stringParsed = new TextDecoder("utf-8").decode(response);
-            const showdownMarkdown = new showdown.Converter().makeHtml(stringParsed);
-            setMarkDown(showdownMarkdown);
-            return function cleanup() {
-                abortController.abort();
-            }
-        })();
-    }, [])
-
-    return (
-        <React.Fragment>
-            <br />
-            <div style={{ display: "flex", flexDirection: 'column' }} >
-                <Container>
-                    <h4>Developers' Guild Roles</h4>
-                    <p className="text-justify">
-                        These are roles currently open at the Developers' Guild.
-                    </p>
-                </Container>
-                <img className="open-source-header lazyload" style={{ width: '30vw', alignSelf: 'center' }} data-src={RolesHeader} alt="Header" />
-                <Container className='role-container'>
-                    <br/>
-                    <br/>
-                    <div dangerouslySetInnerHTML={{ __html: markDown }} ></div>
-                    <a target="__blank" href="https://github.com/DevelopersGuild/developers-guild-site-roles/blob/master/Roles.md">Edit this page here.</a>
-                    <br/>
-                    <br/>
-                </Container>
-            </div>
-
-        </React.Fragment>
+async function fetchRolesMarkdown(): Promise<string> {
+  const response = await (
+    await fetch(
+      "https://raw.githubusercontent.com/DevelopersGuild/developers-guild-site-roles/master/Roles.md"
     )
+  ).arrayBuffer();
+  const parsed = new TextDecoder("utf-8").decode(response);
+  return new showdown.Converter().makeHtml(parsed);
 }
 
+const Roles: React.FC = () => {
+  const { status, data, error } = useQuery("fetch-roles", fetchRolesMarkdown);
 
-/**
- * Memoizing component to prevent useless re-rendering
- */
-const Roles = React.memo(RolesRaw);
+  if (status === "loading") return <div>Loading...</div>;
+  if (status === "error") return <div>Error: {error}</div>;
+
+  return (
+    <React.Fragment>
+      <br />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Container>
+          <h4>Developers' Guild Roles</h4>
+          <p className="text-justify">
+            These are roles currently open at the Developers' Guild.
+          </p>
+        </Container>
+        <img
+          className="open-source-header lazyload"
+          style={{ width: "30vw", alignSelf: "center" }}
+          data-src={RolesHeader}
+          alt="Header"
+        />
+        <Container className="role-container">
+          <br />
+          <br />
+          <div dangerouslySetInnerHTML={{ __html: data ? data : "" }}></div>
+          <a
+            target="__blank"
+            href="https://github.com/DevelopersGuild/developers-guild-site-roles/blob/master/Roles.md"
+          >
+            Edit this page here.
+          </a>
+          <br />
+          <br />
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Roles;
