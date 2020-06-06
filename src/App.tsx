04ab3361bf1a3a8f59025f0e0818ea9a8956efb8
Carlos Alba <soradev4@gmail.com>
2020-06-05 18:24:47 -0700

    Routing Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import { Landing } from "./components/pages/Landing";
 import { Contact } from "./components/pages/Contact";
 import { NavbarMain } from "./components/ui/NavbarMain";
@@ -8,20 +7,7 @@ import { About } from "./components/pages/About";
 import { Contributing } from "./components/pages/Contributing";
 import { Thanks } from "./components/pages/Thanks";
 import Constitution from "./components/pages/Constitution";
-const RolesLazy = lazy(() => import("./components/pages/Roles"));
-
-// independent parsed resources on the network being lazily loaded
-const Roles = () => (
-  <Suspense
-    fallback={
-      <Container>
-        <div>Loading...</div>
-      </Container>
-    }
-  >
-    <RolesLazy />
-  </Suspense>
-);
+import Roles from "./components/pages/Roles";
 
 const App: React.FC = () => {
   return (
