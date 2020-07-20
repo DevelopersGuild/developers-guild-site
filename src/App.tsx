d882c05a9caff2c8ea803a3a95bba2bc9976ef2b
Carlos Alba <soradev4@gmail.com>
2020-07-20 13:13:11 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,11 @@ import CodeDetailView from "./components/pages/Code/CodeDetailView";
 import NewCodeBlock from "./components/pages/Code/NewCodeBlock";
 import Lms from "./lms/pages/Lms";
 import sitedata from "./sitedata";
-import { Helmet } from "react-helmet";
 import ogimagepath from "./components/graphic-assets/og.jpg";
 
 const App: React.FC = () => {
   return (
     <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{sitedata.title}</title>
-        <link rel="canonical" href={sitedata.url} />
-        <meta name="description" content={sitedata.description}></meta>
-        <meta
-          property="og:image"
-          content={`https://da-developers.dev${ogimagepath}`}
-        />
-        <meta property="og:description" content={sitedata.description} />
-        <meta property="og:title" content={sitedata.title} />
-        <meta property="og:type" content="website" />
-      </Helmet>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Landing} />
