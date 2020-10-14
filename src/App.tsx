1d25cff2af9e8ed488b6a76580ef1712d1547358
Carlos Alba <soradev4@gmail.com>
2020-10-13 21:51:52 -0700

    Removing Old Contributing Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
-import { Contributing } from "./pages/Contributing";
 import Lab from "./pages/Lab";
 import Constitution from "./pages/Constitution";
 import Roles from "./pages/Roles";
@@ -15,7 +14,6 @@ const App: React.FC = () => {
         <Route path="/" exact component={Landing} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/about" exact component={About} />
-        <Route path="/contributing" exact component={Contributing} />
         <Route path="/roles" exact component={Roles} />
         <Route path="/constitution" exact component={Constitution} />
         <Route path="/lab" exact component={Lab} />
