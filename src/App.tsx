c58bc29d51d0ae43525a081dd7cade05c96590a8
Carlos Alba <soradev4@gmail.com>
2020-10-13 21:52:17 -0700

    Removing the Roles Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 import Lab from "./pages/Lab";
 import Constitution from "./pages/Constitution";
-import Roles from "./pages/Roles";
 
 const App: React.FC = () => {
   return (
@@ -14,7 +13,6 @@ const App: React.FC = () => {
         <Route path="/" exact component={Landing} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/about" exact component={About} />
-        <Route path="/roles" exact component={Roles} />
         <Route path="/constitution" exact component={Constitution} />
         <Route path="/lab" exact component={Lab} />
       </Switch>
