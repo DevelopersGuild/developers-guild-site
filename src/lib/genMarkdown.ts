c91b5dc6233645d7627ad5d958b95ae6bb506abf
Carlos Alba <soradev4@gmail.com>
2020-10-15 16:21:09 -0700

    genUpdate

--- src/lib/genMarkdown.ts
+++ src/lib/genMarkdown.ts
@@ -1,10 +1,10 @@
 import fetch from 'isomorphic-fetch'
 
-function mxref(filename: string, origin: string) {
+function mxref(filename: string, origin: string): string {
   return "" + origin + "/markdown/" + filename;
 }
 
-function genMarkdown(path: string, origin: string) {
+function genMarkdown(path: string, origin: string): Promise<string> {
   return fetch(mxref(path, origin)).then(function (prim) {
                 return prim.text();
               }).then(function (text) {
