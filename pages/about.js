630101b488d95df06f5acdbac92c6b5b79bff646
Carlos Alba <soradev4@gmail.com>
2023-05-05 21:26:02 -0700

    Modding About Page

--- pages/about.js
+++ pages/about.js
@@ -1,82 +1,48 @@
-import React from "react";
-import Link from "next/link";
-import { StyleSheet, css } from "aphrodite";
 import { Container, Row } from "react-bootstrap";
+import Head from 'next/head'
 
-const renderList = (elements) => (
-  <ul>
-    {elements.map((element, idx) => (
-      <li key={idx}>{element}</li>
-    ))}
-  </ul>
-)
-
-function createSection({ jumbotron = false, list = false, title, body }) {
-  return (
-    <div className={css(styles.section)}>
-      <h2 className="mb-4">{title}</h2>
-      {list ? renderList(body) : <p className="text-left">{body}</p>}
-    </div>
-  );
+// TODO!: Convert to Markdown Page. I was too lazy to do all the legwork on top of Next 13.
+export default function About(props) {
+    return (
+        <>
+            <Head>
+                <link rel="preload" as="image" href="/og.jpg" />
+                <title>Developers Guild | About</title>
+            </Head>
+            <Container>
+                <h1>About The Developers' Guild</h1>
+                <h2>Our Purpose</h2>
+                <p>
+                    Developers’ Guild started in 2014 with the goal of facilitating the exploration of computer science through hands-on experience.
+                    Developers’ Guild creates a casual environment to practice real-world programming. We foster a community of avid programmers by
+                    tackling projects that interest us, participating and competing in events, and gathering skills and experience for the future.
+                    Our members are divided into various groups that focus on a different aspect of computer science, such as video game development,
+                    machine learning/AI, and web application development. Members in these groups will learn these computer science concepts and apply
+                    them in projects, ranging from beginner to advanced difficulties.
+                </p>
+                <h2>Why Join?</h2>
+                <p>
+                    When you join DG and attend our meetings, you can help contribute code and more to existing and new projects that you can add to your portfolio
+                    for future jobs or even transfer. Learn new concepts with a community of people that share a passion for programming!
+                </p>
+                <h2>Open To Everyone</h2>
+                <p>
+                    Even people who aren’t majoring in computer science can still join!
+                    Learn to work with computer programmers and create software that’s appealing to end-users!
+                    There are more aspects of software development than just coding and we at Developers’ Guild understand that.
+                    Create assets for games and UI mockups for websites and applications!
+                </p>
+                <h2>TL;DR</h2>
+                <ul>
+                    <li>No prior experience necessary!</li>
+                    <li>Provides a good way to get real-world programming experience.</li>
+                    <li>Facilitates the exploration of computer science through hands-on experience.</li>
+                    <li>Adept programmers can work on new challenges and mentor others.</li>
+                    <li>Contribute/Create open-source projects for your portfolio and resume!</li>
+                    <li>Collaboration with individuals with different skills, perspectives, and backgrounds.</li>
+                    <li>Communication with mentors, industry leaders, and speakers.</li>
+                </ul>
+            </Container>
+        </>
+    );
 }
-
-export default function About({ sections }) {
-  return (
-    <div className={`${css(styles.biggerFont)} text-center`}>
-      <h1 className="text-center">About The Developers' Guild</h1>
-      <Row className="justify-content-center">
-        <div className="divider" />
-      </Row>
-      <Container>
-        {sections.map(section => {
-          return createSection(section);
-        })}
-      </Container>
-    </div>
-  );
-}
-
-const styles = StyleSheet.create({
-  biggerFont: {
-    fontSize: '1.125rem'
-  },
-  section: {
-    padding: '2.5rem 0 !important'
-  }
-});
-
-export const getStaticProps = async (context) => {
-  return {
-    props: {
-      sections: [
-        {
-          jumbotron: true,
-          title: "Our purpose",
-          body: "Developers’ Guild started in 2014 with the goal of facilitating the exploration of computer science through hands-on experience. Developers’ Guild creates a casual environment to practice real-world programming. We foster a community of avid programmers by tackling projects that interest us, participating and competing in events, and gathering skills and experience for the future. Our members are divided into various groups that focus on a different aspect of computer science, such as video game development, machine learning/AI, and web application development. Members in these groups will learn these computer science concepts and apply them in projects, ranging from beginner to advanced difficulties."
-        },
-        {
-          title: "Why Join?",
-          body: "When you join DG and attend our meetings, you can help contribute code and more to existing and new projects that you can add to your portfolio for future jobs or even transfer. Learn new concepts with a community of people that share a passion for programming!"
-        },
-        {
-          jumbotron: true,
-          title: "Open To Everyone",
-          body: "Even people who aren’t majoring in computer science can still join! Learn to work with computer programmers and create software that’s appealing to end-users! There are more aspects of software development than just coding and we at Developers’ Guild understand that. Create assets for games and UI mockups for websites and applications!"
-        },
-        {
-          list: true,
-          title: "TL;DR",
-          body: [
-            "No prior experience necessary!",
-            "Provides a good way to get real-world programming experience.",
-            "Facilitates the exploration of computer science through hands-on experience.",
-            "Adept programmers can work on new challenges and mentor others.",
-            "Contribute/Create open-source projects for your portfolio and resume!",
-            "Collaboration with individuals with different skills, perspectives and backgrounds.",
-            "Communication with mentors, industry leaders and speakers."
-          ]
-        },
-      ]
-    },
-  };
-};
